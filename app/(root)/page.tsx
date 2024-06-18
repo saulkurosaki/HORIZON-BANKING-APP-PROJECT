1d3995d2fc5b7b24b0c45c0391a4fb4e3f900f31
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-17 21:44:50 -0600

    feat(Home): implement the TotalBalanceBox component within the main component along as its props

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from "@/components/shared/HeaderBox";
+import TotalBalanceBox from "@/components/shared/TotalBalanceBox";
 
 const Home = () => {
   const loggedIn = { firstName: "Saul" };
@@ -12,6 +13,12 @@ const Home = () => {
             title="Welcome"
             user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
           />
         </header>
       </div>
