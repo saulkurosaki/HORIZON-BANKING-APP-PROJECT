7fbf924dc00ca124326b20edd2d9c5661ee9c2a5
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-17 21:48:48 -0600

    feat(TotalBalanceBox): implement the component container layout

--- components/shared/TotalBalanceBox.tsx
+++ components/shared/TotalBalanceBox.tsx
@@ -1,5 +1,15 @@
-const TotalBalanceBox = () => {
-  return <div>TotalBalanceBox</div>;
+const TotalBalanceBox = ({
+  accounts = [],
+  totalBanks,
+  totalCurrentBalance,
+}: TotalBalanceBoxProps) => {
+  return (
+    <section className="total-balance">
+      <div className="total-balance-chart">{/* DoughnutChart */}</div>
+
+      {/* <div className="flex flex-col gap-6"></div> */}
+    </section>
+  );
 };
 
 export default TotalBalanceBox;
