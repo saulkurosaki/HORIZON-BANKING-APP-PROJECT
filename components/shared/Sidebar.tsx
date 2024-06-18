9a3d1503e3168638ba871cf1ef98f13ffcc98c96
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-18 10:02:04 -0600

    feat(Sidebar): implement the complete sidebar links display with all of its properties

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar = ({ user }: SidebarProps) => {
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
-        <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+        <Link href="/" className="flex mb-12 cursor-pointer items-center gap-2">
           <Image
             src="/icons/logo.svg"
             width={34}
@@ -33,7 +33,17 @@ const Sidebar = ({ user }: SidebarProps) => {
               href={item.route}
               className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
             >
-              {item.label}
+              <div className="relative size-6">
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({ "brightness-[3] invert-0": isActive })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
             </Link>
           );
         })}
