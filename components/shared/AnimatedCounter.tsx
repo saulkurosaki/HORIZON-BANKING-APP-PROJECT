486adbe4d37a780e4822f2e86e489dbf08f6a588
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-17 21:56:30 -0600

    feat(AnimatedCounter): implement the AnimatedCounter component initial layout

---
+++ components/shared/AnimatedCounter.tsx
@@ -0,0 +1,5 @@
+const AnimatedCounter = () => {
+  return <div>AnimatedCounter</div>;
+};
+
+export default AnimatedCounter;
--- components/shared/TotalBalanceBox.tsx
+++ components/shared/TotalBalanceBox.tsx
@@ -1,4 +1,5 @@
 import { formatAmount } from "@/lib/utils";
+import AnimatedCounter from "./AnimatedCounter";
 
 const TotalBalanceBox = ({
   accounts = [],
@@ -15,6 +16,8 @@ const TotalBalanceBox = ({
         <div className="flex flex-col gap-2">
           <p className="total-balance-label">Total Current Balance</p>
           <p className="total-balance-amount flex-center gap-2">
+            <AnimatedCounter />
+
             {formatAmount(totalCurrentBalance)}
           </p>
         </div>
