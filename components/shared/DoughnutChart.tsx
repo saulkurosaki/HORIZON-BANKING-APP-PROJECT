67bd7fa57f6cb56eafb283de52044a402175aa36
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-18 09:24:00 -0600

    feat(DoughnutChart): implement the Doughnut component additional options

--- components/shared/DoughnutChart.tsx
+++ components/shared/DoughnutChart.tsx
@@ -17,7 +17,19 @@ const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
     labels: ["Bank 1", "Bank 2", "Bank 3"],
   };
 
-  return <Doughnut data={data} />;
+  return (
+    <Doughnut
+      data={data}
+      options={{
+        cutout: "60%",
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      }}
+    />
+  );
 };
 
 export default DoughnutChart;
