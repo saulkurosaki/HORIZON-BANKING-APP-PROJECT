5bcce2b83f7db47c2e07b25552addc80b01d7984
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-17 22:01:52 -0600

    feat(AnimatedCounter): implement the complete AnimatedCounter component

--- components/shared/AnimatedCounter.tsx
+++ components/shared/AnimatedCounter.tsx
@@ -1,5 +1,13 @@
-const AnimatedCounter = () => {
-  return <div>AnimatedCounter</div>;
+"use client";
+
+import CountUp from "react-countup";
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <div className="w-full">
+      <CountUp end={amount} decimal="," prefix="$" duration={2} decimals={2} />
+    </div>
+  );
 };
 
 export default AnimatedCounter;
--- components/shared/TotalBalanceBox.tsx
+++ components/shared/TotalBalanceBox.tsx
@@ -1,4 +1,3 @@
-import { formatAmount } from "@/lib/utils";
 import AnimatedCounter from "./AnimatedCounter";
 
 const TotalBalanceBox = ({
@@ -16,9 +15,7 @@ const TotalBalanceBox = ({
         <div className="flex flex-col gap-2">
           <p className="total-balance-label">Total Current Balance</p>
           <p className="total-balance-amount flex-center gap-2">
-            <AnimatedCounter />
-
-            {formatAmount(totalCurrentBalance)}
+            <AnimatedCounter amount={totalCurrentBalance} />
           </p>
         </div>
       </div>
