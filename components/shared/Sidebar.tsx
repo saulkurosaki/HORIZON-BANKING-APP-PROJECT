685b84704559092425678ce9650f2bce980b4540
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-18 09:48:57 -0600

    feat(Sidebar): implement the sidebar links display along as the is active styling feature

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,7 +1,43 @@
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const Sidebar = ({ user }: SidebarProps) => {
+  const pathname = usePathname();
+
   return (
     <section className="sidebar">
-      <nav className="flex flex-col gap-4"></nav>
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Horizon Logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Horizon</h1>
+        </Link>
+
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+
+          return (
+            <Link
+              key={item.label}
+              href={item.route}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+            >
+              {item.label}
+            </Link>
+          );
+        })}
+      </nav>
     </section>
   );
 };
