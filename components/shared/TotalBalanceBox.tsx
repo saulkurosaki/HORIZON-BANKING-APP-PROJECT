eff8a33af4e35e7e691dd92768c88898ce2fac46
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-17 21:50:19 -0600

    feat(TotalBalanceBox): implement the total bank accounts display

--- components/shared/TotalBalanceBox.tsx
+++ components/shared/TotalBalanceBox.tsx
@@ -7,7 +7,9 @@ const TotalBalanceBox = ({
     <section className="total-balance">
       <div className="total-balance-chart">{/* DoughnutChart */}</div>
 
-      {/* <div className="flex flex-col gap-6"></div> */}
+      <div className="flex flex-col gap-6">
+        <h2 className="header-2">Bank Accounts: {totalBanks}</h2>
+      </div>
     </section>
   );
 };
