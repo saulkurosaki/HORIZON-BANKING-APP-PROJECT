2262c351cd0521b652413de3ec8052f6f31c3aa9
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-17 21:37:32 -0600

    feat(Home): implement the HeaderBox component within the main component along as its props

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,18 @@
 import HeaderBox from "@/components/shared/HeaderBox";
 
 const Home = () => {
+  const loggedIn = { firstName: "Saul" };
+
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-          <HeaderBox />
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
         </header>
       </div>
     </section>
