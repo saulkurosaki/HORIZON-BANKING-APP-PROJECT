631de04acdbf7bbc88b2fb95117de9cda24f6fe7
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-17 22:14:14 -0600

    feat(DoughnutChart): implement the chartjs initial doughnut chart along as its data prop

--- components/shared/TotalBalanceBox.tsx
+++ components/shared/TotalBalanceBox.tsx
@@ -9,7 +9,7 @@ const TotalBalanceBox = ({
   return (
     <section className="total-balance">
       <div className="total-balance-chart">
-        <DoughnutChart />
+        <DoughnutChart accounts={accounts} />
       </div>
 
       <div className="flex flex-col gap-6">
