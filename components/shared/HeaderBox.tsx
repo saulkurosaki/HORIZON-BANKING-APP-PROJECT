7860f986837dd10e91f0ca01d755aa58e69c8cce
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-17 21:41:50 -0600

    feat(HeaderBox): implement the component title display

--- components/shared/HeaderBox.tsx
+++ components/shared/HeaderBox.tsx
@@ -1,5 +1,19 @@
-const HeaderBox = () => {
-  return <div>HeaderBox</div>;
+const HeaderBox = ({
+  type = "title",
+  title,
+  user,
+  subtext,
+}: HeaderBoxProps) => {
+  return (
+    <div className="header-box">
+      <h1 className="header-box-title">
+        {title}
+        {type === "greeting" && (
+          <span className="text-bankGradient">&nbsp;{user}</span>
+        )}
+      </h1>
+    </div>
+  );
 };
 
 export default HeaderBox;
