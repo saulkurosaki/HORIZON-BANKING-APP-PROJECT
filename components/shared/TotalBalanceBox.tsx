87e5d8d8d304c2a74fc6ad206ac7fa76e0d667ce
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-17 21:53:36 -0600

    feat(TotalBalanceBox): implement the total current balance display

--- components/shared/TotalBalanceBox.tsx
+++ components/shared/TotalBalanceBox.tsx
@@ -1,3 +1,5 @@
+import { formatAmount } from "@/lib/utils";
+
 const TotalBalanceBox = ({
   accounts = [],
   totalBanks,
@@ -9,6 +11,13 @@ const TotalBalanceBox = ({
 
       <div className="flex flex-col gap-6">
         <h2 className="header-2">Bank Accounts: {totalBanks}</h2>
+
+        <div className="flex flex-col gap-2">
+          <p className="total-balance-label">Total Current Balance</p>
+          <p className="total-balance-amount flex-center gap-2">
+            {formatAmount(totalCurrentBalance)}
+          </p>
+        </div>
       </div>
     </section>
   );
