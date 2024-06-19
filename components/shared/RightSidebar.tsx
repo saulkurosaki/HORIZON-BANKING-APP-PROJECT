3e648f9610b6412916f6031ffc627018a3923dcf
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-19 10:21:09 -0600

    feat(RightSidebar): implement the BankCard components along as its props

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -41,12 +41,22 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
         {banks?.length > 0 && (
           <div className="relative flex flex-1 flex-col items-center justify-center gap-5">
             <div className="relative z-10">
-              <BankCard />
+              <BankCard
+                key={banks[0].$id}
+                account={banks[0]}
+                username={`${user.firstName} ${user.lastName}`}
+                showBalance={false}
+              />
             </div>
 
             {banks[1] && (
               <div className="absolute right-0 top-8 z-0 w-[90%]">
-                <BankCard />
+                <BankCard
+                  key={banks[1].$id}
+                  account={banks[1]}
+                  username={`${user.firstName} ${user.lastName}`}
+                  showBalance={false}
+                />
               </div>
             )}
           </div>
