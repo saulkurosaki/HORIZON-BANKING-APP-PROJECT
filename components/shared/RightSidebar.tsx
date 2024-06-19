cbc4842eb1d933ce5509ec294d81487847a5d8d1
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-19 09:57:31 -0600

    feat(RightSidebar): implement the component container layout

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,5 +1,9 @@
-const RightSidebar = () => {
-  return <div>RightSidebar</div>;
+const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
+  return (
+    <aside className="right-sidebar">
+      <section className="flex fle-col pb-8"></section>
+    </aside>
+  );
 };
 
 export default RightSidebar;
