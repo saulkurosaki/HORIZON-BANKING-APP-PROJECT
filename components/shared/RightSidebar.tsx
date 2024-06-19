2d91807a7c6e1056610d56860ddbf32709ae6ca4
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-19 10:05:21 -0600

    feat(RightSidebar): implement the profile user first letter as the profile image

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,8 +1,16 @@
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
   return (
     <aside className="right-sidebar">
-      <section className="flex fle-col pb-8">
+      <section className="flex flex-col pb-8">
         <div className="profile-banner" />
+
+        <div className="profile">
+          <div className="profile-img">
+            <span className="text-5xl font-bold text-blue-500">
+              {user.firstName[0]}
+            </span>
+          </div>
+        </div>
       </section>
     </aside>
   );
