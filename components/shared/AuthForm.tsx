32b5594e8d905ffbfd340f12754db686a2b9f0de
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-19 15:35:37 -0600

    feat(AuthForm): implement the component horizon logo

--- components/shared/AuthForm.tsx
+++ components/shared/AuthForm.tsx
@@ -1,5 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const AuthForm = ({ type }: { type: string }) => {
-  return <div>{type}</div>;
+  return (
+    <section className="auth-form">
+      <header className="flex flex-col gap-5 md:gap-8">
+        <Link href="/" className="flex cursor-pointer items-center gap-1">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Horizon Logo"
+          />
+          <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+            Horizon
+          </h1>
+        </Link>
+      </header>
+    </section>
+  );
 };
 
 export default AuthForm;
