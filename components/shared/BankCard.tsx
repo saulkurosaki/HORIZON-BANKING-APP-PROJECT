7d75e059beef9f29bc9405702b9447f11a9c6cf3
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-19 10:27:40 -0600

    feat(BankCard): implement the account name or username display

--- components/shared/BankCard.tsx
+++ components/shared/BankCard.tsx
@@ -1,9 +1,23 @@
+import Link from "next/link";
+
 const BankCard = ({
   account,
   userName,
   showBalance = true,
 }: CreditCardProps) => {
-  return <div className="flex flex-col"></div>;
+  return (
+    <div className="flex flex-col">
+      <Link href="/" className="bank-card">
+        <div className="bank-card_content">
+          <div>
+            <h1 className="text-16 font-semibold text-white">
+              {account.name || userName}
+            </h1>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default BankCard;
