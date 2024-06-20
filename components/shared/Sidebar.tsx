c2377e61d889bc4d8780b0d2f4b96d6f296cc37c
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-19 22:32:20 -0600

    feat(Footer): implement the Footer component initial layout

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Footer from "./Footer";
 
 const Sidebar = ({ user }: SidebarProps) => {
   const pathname = usePathname();
@@ -48,7 +49,8 @@ const Sidebar = ({ user }: SidebarProps) => {
         })}
         USER
       </nav>
-      FOOTER
+
+      <Footer user={user} />
     </section>
   );
 };
