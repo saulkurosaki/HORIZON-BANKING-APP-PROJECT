1e4c584cb3354c9ae33658c8499bf1d1eadd056d
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-20 15:32:38 -0600

    feat(Footer): implement the user full name and email display

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -6,7 +6,20 @@ const Footer = ({ user, type = "desktop" }: FooterProps) => {
           type === "mobile" ? "footer_name-mobile" : "footer_name"
         }`}
       >
-        <p className="text-xl font-bold text-gray-700">{user.firstName[0]}</p>
+        <p className="text-xl font-bold text-gray-700">{user.name[0]}</p>
+      </div>
+
+      <div
+        className={`${
+          type === "mobile" ? "footer_email-mobile" : "footer_email"
+        }`}
+      >
+        <h1 className="text-14 truncate font-semibold text-gray-700">
+          {user.name}
+        </h1>
+        <p className="text-14 truncate font-normal text-gray-600">
+          {user.email}
+        </p>
       </div>
     </footer>
   );
