e7218e474ea01d37c7b57737ef7c0f06083e2c1e
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-20 15:16:00 -0600

    feat(Footer): implement the component container layout along as the first letter of the users name display

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,15 @@
-const Footer = () => {
-  return <div>Footer</div>;
+const Footer = ({ user, type = "desktop" }: FooterProps) => {
+  return (
+    <footer className="footer">
+      <div
+        className={`${
+          type === "mobile" ? "footer_name-mobile" : "footer_name"
+        }`}
+      >
+        <p className="text-xl font-bold text-gray-700">{user.firstName[0]}</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
