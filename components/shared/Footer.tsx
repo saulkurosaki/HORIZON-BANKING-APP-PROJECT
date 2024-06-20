ddb1e3511e48b9a02f3c18ee2450a5a8b4d729e9
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-20 15:40:35 -0600

    feat(Footer): implement the logout button along as the handleLogOut async function

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,4 +1,16 @@
+import { logoutAccount } from "@/lib/actions/user.actions";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
 const Footer = ({ user, type = "desktop" }: FooterProps) => {
+  const router = useRouter();
+
+  const handleLogOut = async () => {
+    const loggedOut = await logoutAccount();
+
+    if (loggedOut) router.push("/sign-in");
+  };
+
   return (
     <footer className="footer">
       <div
@@ -20,6 +32,10 @@ const Footer = ({ user, type = "desktop" }: FooterProps) => {
         <p className="text-14 truncate font-normal text-gray-600">
           {user.email}
         </p>
+      </div>
+
+      <div className="footer_image" onClick={handleLogOut}>
+        <Image src="/icons/logout.svg" alt="logout" />
       </div>
     </footer>
   );
