f6cb4b66cff1223a553c4b52ab71e64e0b5ae06c
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-21 17:46:13 -0600

    feat(PlaidLink): implement the connect bank buttons when different variant values initial layout

--- components/shared/PlaidLink.tsx
+++ components/shared/PlaidLink.tsx
@@ -1,5 +1,17 @@
-const PlaidLink = () => {
-  return <div>PlaidLink</div>;
+import { Button } from "../ui/button";
+
+const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+  return (
+    <>
+      {variant === "primary" ? (
+        <Button className="plaidlink-primary">Connect Bank</Button>
+      ) : variant === "ghost" ? (
+        <Button>Connect Bank</Button>
+      ) : (
+        <Button>Connect Bank</Button>
+      )}
+    </>
+  );
 };
 
 export default PlaidLink;
