4c2964921ee07c42e37fae2857aa2d4a30a9ae2e
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-21 18:04:43 -0600

    feat(user.actions.ts): implement the complete createLinkToken server action function

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,8 @@ import { ID } from "node-appwrite";
 import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
 import { parseStringify } from "../utils";
+import { CountryCode, Products } from "plaid";
+import { plaidClient } from "../plaid";
 
 export const signIn = async ({ email, password }: signInProps) => {
   try {
@@ -67,5 +69,25 @@ export const logoutAccount = async () => {
     await account.deleteSession("current");
   } catch (error) {
     return null;
+  }
+};
+
+export const createLinkToken = async (user: User) => {
+  try {
+    const tokenParams = {
+      user: {
+        client_user_id: user.$id,
+      },
+      client_name: user.name,
+      products: ["auth"] as Products[],
+      language: "en",
+      country_codes: ["US"] as CountryCode[],
+    };
+
+    const response = await plaidClient.linkTokenCreate(tokenParams);
+
+    return parseStringify({ linkToken: response.data.link_token });
+  } catch (error) {
+    console.log(error);
   }
 };
