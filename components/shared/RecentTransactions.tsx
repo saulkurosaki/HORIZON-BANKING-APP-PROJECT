4bf4f422c5f682e53aaf6afb9c19dee51e24b19d
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-24 17:30:35 -0600

    feat(RecentTransactions): implement the recent transactions section header

--- components/shared/RecentTransactions.tsx
+++ components/shared/RecentTransactions.tsx
@@ -1,5 +1,24 @@
-const RecentTransactions = () => {
-  return <div>RecentTransactions</div>;
+import Link from "next/link";
+
+const RecentTransactions = ({
+  accounts,
+  transactions = [],
+  appwriteItemId,
+  page = 1,
+}: RecentTransactionsProps) => {
+  return (
+    <section className="recent-transactions">
+      <header className="flex items-center justify-between">
+        <h2 className="recent-transactions-label">Recent Transactions</h2>
+        <Link
+          href={`transaction-history/?id=${appwriteItemId}`}
+          className="view-all-btn"
+        >
+          View all
+        </Link>
+      </header>
+    </section>
+  );
 };
 
 export default RecentTransactions;
