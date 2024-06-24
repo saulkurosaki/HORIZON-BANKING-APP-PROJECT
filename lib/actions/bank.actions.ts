68f2e2cf7ad13fd29d50967396ab4b56b2603846
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-24 07:53:49 -0600

    feat(bank.actions.ts): implement the complete getAccount server action function

--- lib/actions/bank.actions.ts
+++ lib/actions/bank.actions.ts
@@ -64,69 +64,69 @@ export const getAccounts = async ({ userId }: getAccountsProps) => {
 };
 
 // Get one bank account
-// export const getAccount = async ({ appwriteItemId }: getAccountProps) => {
-//   try {
-//     // get bank from db
-//     const bank = await getBank({ documentId: appwriteItemId });
+export const getAccount = async ({ appwriteItemId }: getAccountProps) => {
+  try {
+    // get bank from db
+    const bank = await getBank({ documentId: appwriteItemId });
 
-//     // get account info from plaid
-//     const accountsResponse = await plaidClient.accountsGet({
-//       access_token: bank.accessToken,
-//     });
-//     const accountData = accountsResponse.data.accounts[0];
+    // get account info from plaid
+    const accountsResponse = await plaidClient.accountsGet({
+      access_token: bank.accessToken,
+    });
+    const accountData = accountsResponse.data.accounts[0];
 
-//     // get transfer transactions from appwrite
-//     const transferTransactionsData = await getTransactionsByBankId({
-//       bankId: bank.$id,
-//     });
+    // get transfer transactions from appwrite
+    const transferTransactionsData = await getTransactionsByBankId({
+      bankId: bank.$id,
+    });
 
-//     const transferTransactions = transferTransactionsData.documents.map(
-//       (transferData: Transaction) => ({
-//         id: transferData.$id,
-//         name: transferData.name!,
-//         amount: transferData.amount!,
-//         date: transferData.$createdAt,
-//         paymentChannel: transferData.channel,
-//         category: transferData.category,
-//         type: transferData.senderBankId === bank.$id ? "debit" : "credit",
-//       })
-//     );
+    const transferTransactions = transferTransactionsData.documents.map(
+      (transferData: Transaction) => ({
+        id: transferData.$id,
+        name: transferData.name!,
+        amount: transferData.amount!,
+        date: transferData.$createdAt,
+        paymentChannel: transferData.channel,
+        category: transferData.category,
+        type: transferData.senderBankId === bank.$id ? "debit" : "credit",
+      })
+    );
 
-//     // get institution info from plaid
-//     const institution = await getInstitution({
-//       institutionId: accountsResponse.data.item.institution_id!,
-//     });
+    // get institution info from plaid
+    const institution = await getInstitution({
+      institutionId: accountsResponse.data.item.institution_id!,
+    });
 
-//     const transactions = await getTransactions({
-//       accessToken: bank?.accessToken,
-//     });
+    const transactions = await getTransactions({
+      accessToken: bank?.accessToken,
+    });
 
-//     const account = {
-//       id: accountData.account_id,
-//       availableBalance: accountData.balances.available!,
-//       currentBalance: accountData.balances.current!,
-//       institutionId: institution.institution_id,
-//       name: accountData.name,
-//       officialName: accountData.official_name,
-//       mask: accountData.mask!,
-//       type: accountData.type as string,
-//       subtype: accountData.subtype! as string,
-//       appwriteItemId: bank.$id,
-//     };
+    const account = {
+      id: accountData.account_id,
+      availableBalance: accountData.balances.available!,
+      currentBalance: accountData.balances.current!,
+      institutionId: institution.institution_id,
+      name: accountData.name,
+      officialName: accountData.official_name,
+      mask: accountData.mask!,
+      type: accountData.type as string,
+      subtype: accountData.subtype! as string,
+      appwriteItemId: bank.$id,
+    };
 
-//     // sort transactions by date such that the most recent transaction is first
-//     const allTransactions = [...transactions, ...transferTransactions].sort(
-//       (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-//     );
+    // sort transactions by date such that the most recent transaction is first
+    const allTransactions = [...transactions, ...transferTransactions].sort(
+      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+    );
 
-//     return parseStringify({
-//       data: account,
-//       transactions: allTransactions,
-//     });
-//   } catch (error) {
-//     console.error("An error occurred while getting the account:", error);
-//   }
-// };
+    return parseStringify({
+      data: account,
+      transactions: allTransactions,
+    });
+  } catch (error) {
+    console.error("An error occurred while getting the account:", error);
+  }
+};
 
 // Get bank info
 // export const getInstitution = async ({
