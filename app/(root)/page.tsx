f28ff7e3c082422b237c64ace82aa8bbe38cf0b2
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-24 08:44:07 -0600

    feat(Home): implement the RecentsTransactions component along as its props

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,6 +6,7 @@ import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
   const accounts = await getAccounts({ userId: loggedIn.$id });
 
@@ -34,7 +35,12 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
           />
         </header>
 
-        <RecentTransactions />
+        <RecentTransactions
+          accounts={accountsData}
+          transactions={accounts?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
       </div>
 
       <RightSidebar
