9245b38f328aabb10b94999e9ad4c4675bf73377
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-24 08:06:01 -0600

    feat(user.actions.ts): implement the complete getBank server action function

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -242,3 +242,19 @@ export const getBanks = async ({ userId }: getBanksProps) => {
     console.log(error);
   }
 };
+
+export const getBank = async ({ documentId }: getBankProps) => {
+  try {
+    const { database } = await createAdminClient();
+
+    const bank = await database.listDocuments(
+      DATABASE_ID!,
+      BANK_COLLECTION_ID!,
+      [Query.equal("$id", [documentId])]
+    );
+
+    return parseStringify(bank.documents[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
