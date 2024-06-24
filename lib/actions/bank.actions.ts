be560ef1cb9a051e80db41567c41aa716b921208
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-24 07:54:24 -0600

    feat(bank.actions.ts): implement the complete getTransactions server action function

--- lib/actions/bank.actions.ts
+++ lib/actions/bank.actions.ts
@@ -147,42 +147,42 @@ export const getInstitution = async ({
 };
 
 // Get transactions
-// export const getTransactions = async ({
-//   accessToken,
-// }: getTransactionsProps) => {
-//   let hasMore = true;
-//   let transactions: any = [];
+export const getTransactions = async ({
+  accessToken,
+}: getTransactionsProps) => {
+  let hasMore = true;
+  let transactions: any = [];
 
-//   try {
-//     // Iterate through each page of new transaction updates for item
-//     while (hasMore) {
-//       const response = await plaidClient.transactionsSync({
-//         access_token: accessToken,
-//       });
+  try {
+    // Iterate through each page of new transaction updates for item
+    while (hasMore) {
+      const response = await plaidClient.transactionsSync({
+        access_token: accessToken,
+      });
 
-//       const data = response.data;
+      const data = response.data;
 
-//       transactions = response.data.added.map((transaction) => ({
-//         id: transaction.transaction_id,
-//         name: transaction.name,
-//         paymentChannel: transaction.payment_channel,
-//         type: transaction.payment_channel,
-//         accountId: transaction.account_id,
-//         amount: transaction.amount,
-//         pending: transaction.pending,
-//         category: transaction.category ? transaction.category[0] : "",
-//         date: transaction.date,
-//         image: transaction.logo_url,
-//       }));
+      transactions = response.data.added.map((transaction) => ({
+        id: transaction.transaction_id,
+        name: transaction.name,
+        paymentChannel: transaction.payment_channel,
+        type: transaction.payment_channel,
+        accountId: transaction.account_id,
+        amount: transaction.amount,
+        pending: transaction.pending,
+        category: transaction.category ? transaction.category[0] : "",
+        date: transaction.date,
+        image: transaction.logo_url,
+      }));
 
-//       hasMore = data.has_more;
-//     }
+      hasMore = data.has_more;
+    }
 
-//     return parseStringify(transactions);
-//   } catch (error) {
-//     console.error("An error occurred while getting the accounts:", error);
-//   }
-// };
+    return parseStringify(transactions);
+  } catch (error) {
+    console.error("An error occurred while getting the accounts:", error);
+  }
+};
 
 // Create Transfer
 // export const createTransfer = async () => {
