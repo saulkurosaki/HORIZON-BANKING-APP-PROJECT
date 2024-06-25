907c9dadbde2af24bdc677fac97bcdf952de232d
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-25 16:32:20 -0600

    feat(TransactionHistory): implement the component header section

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,5 +1,16 @@
+import HeaderBox from "@/components/shared/HeaderBox";
+
 const TransactionHistory = () => {
-  return <div>TransactionHistory</div>;
+  return (
+    <section className="transactions">
+      <div className="transactions-header">
+        <HeaderBox
+          title="Transaction History"
+          subtext="See your bank details and transactions."
+        />
+      </div>
+    </section>
+  );
 };
 
 export default TransactionHistory;
