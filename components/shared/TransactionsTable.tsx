e9a281563c49363f2f36771c43b21308cad03b0e
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-24 20:24:06 -0600

    feat(TransactionsTable): implement the complete TableHeader component

--- components/shared/TransactionsTable.tsx
+++ components/shared/TransactionsTable.tsx
@@ -11,23 +11,24 @@ import {
 const TransactionsTable = () => {
   return (
     <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
+      <TableHeader className="bg-[#f9fafb]">
         <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
+          <TableHead className="px-2">Transaction</TableHead>
+          <TableHead className="px-2">Amount</TableHead>
+          <TableHead className="px-2">Status</TableHead>
+          <TableHead className="px-2">Date</TableHead>
+          <TableHead className="px-2 max-md:hidden">Channel</TableHead>
+          <TableHead className="px-2 max-md:hidden">Category</TableHead>
         </TableRow>
       </TableHeader>
-      <TableBody>
+      {/* <TableBody>
         <TableRow>
           <TableCell className="font-medium">INV001</TableCell>
           <TableCell>Paid</TableCell>
           <TableCell>Credit Card</TableCell>
           <TableCell className="text-right">$250.00</TableCell>
         </TableRow>
-      </TableBody>
+      </TableBody> */}
     </Table>
   );
 };
