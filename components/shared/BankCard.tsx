c29142a3f9ce1e375c88454b494bc0cb0111ea2f
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-26 10:20:53 -0600

    feat(BankCard): implement the complete BankCard component

--- components/shared/BankCard.tsx
+++ components/shared/BankCard.tsx
@@ -2,18 +2,23 @@ import { formatAmount } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+// import Copy from "./Copy";
 
 const BankCard = ({
   account,
   userName,
   showBalance = true,
 }: CreditCardProps) => {
+  console.log(account);
   return (
     <div className="flex flex-col">
-      <Link href="/" className="bank-card">
+      <Link
+        href={`/transaction-history/?id=${account.appwriteItemId}`}
+        className="bank-card"
+      >
         <div className="bank-card_content">
           <div>
-            <h1 className="text-16 font-semibold text-white">{userName}</h1>
+            <h1 className="text-16 font-semibold text-white">{account.name}</h1>
             <p className="font-ibm-plex-serif font-black text-white">
               {formatAmount(account.currentBalance)}
             </p>
@@ -25,7 +30,7 @@ const BankCard = ({
               <h2 className="text-12 font-semibold text-white">●● / ●●</h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
-              ●●●● ●●●● ●●●● <span className="text-16">1234</span>
+              ●●●● ●●●● ●●●● <span className="text-16">{account?.mask}</span>
             </p>
           </article>
         </div>
@@ -50,7 +55,7 @@ const BankCard = ({
         />
       </Link>
 
-      {/* COPY */}
+      {/* {showBalance && <Copy title={account?.shareableId} />} */}
     </div>
   );
 };
